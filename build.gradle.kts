e9358020b3d908a0a1e5fc734b4d734b9f14de1a
T1mo2082 <149088583+T1mo2082@users.noreply.github.com>
2025-07-24 21:48:29 +0200

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,11 @@ buildscript {
         jcenter()
     }
     dependencies {
+        // Add this for ConstraintLayout support
+        implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+
+        // Add this for Google's Material Design 3 components
+        implementation 'com.google.android.material:material:1.11.0'
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath(kotlin("gradle-plugin", version = "1.4.30"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3")
